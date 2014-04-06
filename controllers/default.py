ffe413338ba0d1f20e7068bbfec2e75649580a25
Chase Cummings <chaseecummings@gmail.com>
2014-04-06 10:25:29 -0700

    testing

--- controllers/default.py
+++ controllers/default.py
@@ -82,16 +82,16 @@ def edit_profile():
 
 @auth.requires_login()
 def edit_tags() :
-	
+
 	form2 = SQLFORM.factory (Field('tags'),submit_button = 'Update Tags')
 	form2.vars.tags = 'List a current tag to delete it.'
-	
+
 	list = []
 	tags = []
 	record = db(db.profile.owner_id == get_user_id()).select()
 	if (record != None and record.first() != None and record.first().tags != None):
 		tags = record.first().tags
-			
+
 	if (form2.process().accepted):
 		list = form2.vars.tags.split(',') or None
 		for tag in list:
@@ -102,12 +102,12 @@ def edit_tags() :
 				tags.append(tag)
 	else:
 		session.flash = T('Check for errors in form.')
-	
+
 	if (not tags):
 		tags = "No tags yet! Add some above."
 	elif(record.first() != None):
 		record.first().update_record(tags=tags)
-		
+
 	return dict(form2 = form2,tags=tags)
 
 @auth.requires_login()
@@ -138,7 +138,7 @@ def edit_event():
 		redirect(URL('default','wall'))
 	form = SQLFORM(db.events, record = e, fields=['title','start_time','end_time','all_day','image','details'],
 				   submit_button = 'Update Event', showid = False)
-	
+
 	form2 = SQLFORM.factory (Field('tags'),submit_button = 'Update Tags')
 	form2.vars.tags = 'Enter Comma Separated List'
 	list = []
@@ -146,7 +146,7 @@ def edit_event():
 	record = db.events[e]
 	if (record != None and record.tags != None):
 		tags = record.tags
-			
+
 	if (form2.process().accepted):
 		list = form2.vars.tags.split(',') or None
 		for tag in list:
@@ -155,12 +155,12 @@ def edit_event():
 				tags.append(tag.lower())
 	else:
 		session.flash = T('Check for errors in form.')
-		
+
 	if (not tags):
 		tags = "No tags yet! Add some above."
 	else:
 		record.update_record(tags=tags)
-		
+
 	return dict(form = form, form2 = form2,tags=tags)
 
 def new_event():
@@ -243,10 +243,10 @@ def search():
 	results = None
 	list_results_html = None
 	cal_results_html = None
-	
+
 	# Search form
 	search = FORM(INPUT(_name='search', _value='Search Events', _onblur="if(this.value == ''){this.value = 'Search Events'}", _onFocus="if(this.value=='Search Events'){this.value=''}", requires=IS_NOT_EMPTY()), INPUT(_type='submit', _action=URL('search')))
-	
+
 	if (r_temp == None):
 		redirect(URL('default','wall'))
 	else:
@@ -255,11 +255,11 @@ def search():
 		list_results_html = list_format(results)
 		cal_results_html = cal_format(results)
 		return dict(search=search, list_results=P(list_results_html), cal_results=SCRIPT(cal_results_html, _type='text/javascript'))
-	
+
 	# Redirect with search form value
 	if (request.post_vars.search != None):
 		redirect(URL('default','search', args=[request.post_vars.search]))
-	
+
 	return dict(search=None, list_results=None,
 					cal_results=None)
 
@@ -278,7 +278,7 @@ def search_date():
     print "Conflicts", conflicts
     cal_results_html = cal_format(conflicts)
     print SCRIPT(cal_results_html, _type='text/javascript')
-    return dict()
+    return cal_results_html
 
 def view_event():
     if request.args == []:
